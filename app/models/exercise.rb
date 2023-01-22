691f4c52ccbd7027207d917949214840970fda33
trevor dewindt <trevor.dewindt@gmail.com>
2023-01-21 17:54:45 -0700

    add active record associations

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,4 @@
 class Exercise < ActiveRecord::Base
+    has_many :routines
+    has_many :users, through: :routines
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+    has_many :routines
+    has_many :exercises, through: :routines
+
 end
