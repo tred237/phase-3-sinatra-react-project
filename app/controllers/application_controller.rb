d6266dfd02d909615ad57f0a90cb4d8725d08345
trevor dewindt <trevor.dewindt@gmail.com>
2023-01-22 16:07:05 -0700

    remove exercises route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,6 @@ class ApplicationController < Sinatra::Base
     Client.all.to_json
   end
 
-  get "/exercises" do
-    Exercise.all.to_json
-  end
-
   get "/routines" do
     Routine.all.to_json
   end
@@ -21,13 +17,6 @@ class ApplicationController < Sinatra::Base
     routine = Routine.find(params[:id])
     routine.destroy
     routine.to_json
-  end
-
-  delete "/exercises/:id" do
-    exercise = Exercise.find(params[:id])
-    Routine.where("exercise_id = #{params[:id]}").each{|e| e.destroy}
-    exercise.destroy
-    exercise.to_json
   end
 
   delete "/clients/:id" do
